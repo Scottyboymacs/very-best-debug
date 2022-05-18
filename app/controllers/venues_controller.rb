932a8bf5b378cf2b0040fa5d5c18beb186a7cdd7
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-05-18 20:36:19 +0000

    LMaost done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,8 @@
 class VenuesController < ApplicationController
 
   def index
-    matching_venues = venue.all
-    venues = matching_venues.order(:created_at)
+    matching_venues = Venue.all
+    @venues = matching_venues.order(:created_at)
 
     render({ :template => "venue_templates/venue_list.html.erb" })
   end
@@ -10,7 +10,7 @@ class VenuesController < ApplicationController
   def show
     venue_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    @the_venue = matching_venues.first
 
     render({ :template => "venue_templates/details.html.erb" })
   end
@@ -28,9 +28,9 @@ class VenuesController < ApplicationController
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
+    venue = Venue.where({ :id => the_id }).first
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
     
@@ -39,9 +39,9 @@ class VenuesController < ApplicationController
 
   def destroy
     the_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => the_id })
+    matching_venues = Venue.where({ :id => the_id }).first
     venue = matching_venues
-    venue.destroy
+    venue.delete
 
     redirect_to("/venues")
   end
